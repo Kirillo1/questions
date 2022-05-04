662535938a491cda6114ebee28f135fd620b847b
ghost <kirill.han011@gmail.com>
2022-05-04 23:27:02 +0600

    добавлены модели Poll и Choice зарегистрированы в админ панеле добавлен супер юзер созданы и применены миграции

--- poll/admin.py
+++ poll/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from poll.models import Poll, Choice
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'time_of_creations']
+    search_fields = ['question']
+    fields = ['question']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
+class BaseModel(models.Model):
+    time_of_creations = models.DateTimeField(auto_now_add=True, verbose_name='time of creations')
+
+    class Meta:
+        abstract = True
+
+    def get_absolute_url(self):
+        return reverse("poll_view", kwargs={'pk': self.pk})
+
+
+class Poll(BaseModel):
+    question = models.CharField(max_length=100, verbose_name="question")
+
+    def __str__(self):
+        return f"{self.pk}: {self.question}"
+
+    class Meta:
+        db_table = 'poll'
+        verbose_name = 'Poll'
+        verbose_name_plural = 'Polls'
+
+
+class Choice(BaseModel):
+    variant_text = models.CharField(max_length=100, verbose_name="variant text")
+    poll = models.ForeignKey("poll.Poll", on_delete=models.CASCADE, related_name="choices", verbose_name="Choice")
+
+    class Meta:
+        db_table = 'choice'
+        verbose_name = 'Choice'
+        verbose_name_plural = 'Choices'
+
+    # def get_absolute_url(self):
+    #     return reverse("poll_view", kwargs={'pk': self.pk})
+
