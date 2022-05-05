0e4cfba104e09d73305e8f657fc8495c058ff742
ghost <kirill.han011@gmail.com>
2022-05-06 00:12:07 +0600

    реализован детальный просмотр вопросв

--- poll/urls.py
+++ poll/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from poll.views.polls import IndexView
+from poll.views.polls import IndexView, PollDetailView
 
 app_name = "poll"
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('poll/<int:pk>/', PollDetailView.as_view(), name="pool_detail_view"),
 ]
--- poll/views/polls.py
+++ poll/views/polls.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from poll.models import Poll
 
@@ -12,3 +12,21 @@ class IndexView(ListView):
 
     def get_queryset(self):
         return Poll.objects.order_by('-time_of_creations')
+
+
+class PollDetailView(DetailView):
+    template_name = 'polls/poll_detail_view.html'
+    model = Poll
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     count = self.object.answers.count()
+    #     context["count"] = count
+    #     context["choices"] = self.get_choices_with_answers_count(count, self.object.choices.all())
+    #     return context
+    #
+    # def get_choices_with_answers_count(self, count, choices):
+    #     f = 100 / count
+    #     percent = E(F("answer_count") * f, output_field=DecimalField())
+    #     res_choices = choices.annotate(answer_count=Count("answers__pk")).annotate(percent=percent)
+    #     return res_choices
